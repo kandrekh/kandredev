5e07b3dac1d26a858e894b9dcbcdfd159f11c87c
K-Andre Harris <dre@sustainible.io>
2025-07-09 13:04:58 -0400

    `Remove unused blog posts and update About page with refreshed content and styles`

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'K-Andre';
+export const SITE_DESCRIPTION = 'Poetry, Pixels, and Purpose — Exploring the craft of storytelling through words, visuals, and code.';
